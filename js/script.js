56233ce7a620cc5ac3cc28c148f803a780513032
Githubshakil <shakilmahmud007@gmail.com>
2025-05-02 15:00:02 +0600

    drawBricks

--- js/script.js
+++ js/script.js
@@ -112,4 +112,22 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status === 1) {
+                const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+                const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = '#f00';
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
 
