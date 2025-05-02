36085ed72d2dbf52c69e795f5ceefe47c431db40
Githubshakil <shakilmahmud007@gmail.com>
2025-05-02 14:58:34 +0600

    drawBall

--- js/script.js
+++ js/script.js
@@ -95,3 +95,12 @@ function collisionDetection() {
         }
     }
 }
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ctx.fillStyle = '#fff';
+    ctx.fill();
+    ctx.closePath();
+}
+
